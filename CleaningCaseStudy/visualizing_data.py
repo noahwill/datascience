04f2862bee9753931bca91e4a6e49ad414b5b13a
Noah Lawrence <noahwlawrence@gmail.com>
2018-01-16 12:55:46 -0600

    Create visualizing_data.py

---
+++ CleaningCaseStudy/visualizing_data.py
@@ -0,0 +1,17 @@
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Create the scatter plot
+g18002016.plot(kind='scatter', x='1800', y='2016')
+
+# Specify axis labels
+plt.xlabel('Life Expectancy by Country in 1800')
+plt.ylabel('Life Expectancy by Country in 2016')
+
+# Specify axis limits
+# Remember the descriptive statistics from the README? This is where the MAX/MIN can be found.
+plt.xlim(20, 55)
+plt.ylim(20, 90)
+
+# Display the plot
+plt.show()
