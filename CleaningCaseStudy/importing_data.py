890c77597e26b6ec3a40c9c0a7fe5f36203dd6d2
Noah Lawrence <noahwlawrence@gmail.com>
2018-01-16 12:58:18 -0600

    Update importing_data.py

--- CleaningCaseStudy/importing_data.py
+++ CleaningCaseStudy/importing_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-filename = ‘g18002016.csv’
+filename = 'g18002016.csv'
 
 # header gives which row to use as column labels, sep gives the delimeter, na_values gives string recognized as missing values
-data = pd.read_csv(filename, header=0, sep=‘,’, na_values=[“NaN”])
+data = pd.read_csv(filename, header=0, sep=',', na_values=['NaN'])
