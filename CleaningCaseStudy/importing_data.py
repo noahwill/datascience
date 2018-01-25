9a327e53f636ee624db157146989d2415086a162
Noah Lawrence <noahwlawrence@gmail.com>
2018-01-25 14:19:28 -0600

    Update importing_data.py

--- CleaningCaseStudy/importing_data.py
+++ CleaningCaseStudy/importing_data.py
@@ -7,7 +7,7 @@ data = pd.ExcelFile(filename)
 
 # the .parse() method will select the correct sheet from the loaded .xlsx file
 # three dataframes are loaded: Life Expectancy for the 1800s, 1900s, and 2000s
-df_le1800 = data.parse('1800s')
-df_le1900 = data.parse('1900s')
-df_le2000 = data.parse('2000s')
+df_le1800 = data.parse('1800s', head=0)
+df_le1900 = data.parse('1900s', head=0)
+df_le2000 = data.parse('2000s', head=0)
 
