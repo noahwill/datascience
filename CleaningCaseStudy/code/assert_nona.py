b239473ff587d312a12cdfd70bf17add97dafdc0
Noah Lawrence <noahwlawrence@gmail.com>
2018-01-30 09:20:34 -0600

    Create assert_nona.py

---
+++ CleaningCaseStudy/code/assert_nona.py
@@ -0,0 +1,11 @@
+# Assert that country does not contain any missing values
+assert pd.notnull(gapminder.country).all()
+
+# Assert that year does not contain any missing values
+assert pd.notnull(gapminder.year).all()
+
+# Drop the missing values
+gapminder = gapminder.dropna()
+
+# Print the shape of gapminder
+print(gapminder.shape)
