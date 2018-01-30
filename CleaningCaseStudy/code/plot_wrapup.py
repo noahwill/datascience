c9462cd8087f7cfe39cf27fc5fa3ff532ffeb258
Noah Lawrence <noahwlawrence@gmail.com>
2018-01-30 10:24:45 -0600

    Update plot_wrapup.py

--- CleaningCaseStudy/code/plot_wrapup.py
+++ CleaningCaseStudy/code/plot_wrapup.py
@@ -1,11 +1,12 @@
+#PLOT_WRAPUP.PY
 # Add first subplot
 plt.subplot(2, 1, 1) 
 
 # Create a histogram of life_expectancy
-life_expectancy = gapminder.life_expectancy.plot(kind='hist')
+life_expectancy = gapminder_melt.life_expectancy.plot(kind='hist')
 
 # Group gapminder: gapminder_agg
-gapminder_agg = gapminder.groupby('year')['life_expectancy'].mean()
+gapminder_agg = gapminder_melt.groupby('year')['life_expectancy'].mean()
 
 # Print the head of gapminder_agg
 print(gapminder_agg.head())
@@ -29,5 +30,5 @@ plt.tight_layout()
 plt.show()
 
 # Save both DataFrames to csv files
-gapminder.to_csv('gapminder.csv')
-gapminder_agg.to_csv('gapminder_agg.csv')
+gapminder_melt.to_csv('gapminder.csv', encoding='utf8')
+gapminder_agg.to_csv('gapminder_agg.csv', encoding='utf8')
