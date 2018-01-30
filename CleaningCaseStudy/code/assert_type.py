d73ccfa7cc7a0e3d0364789c5f8492020d865f98
Noah Lawrence <noahwlawrence@gmail.com>
2018-01-30 10:23:26 -0600

    Update assert_type.py

--- CleaningCaseStudy/code/assert_type.py
+++ CleaningCaseStudy/code/assert_type.py
@@ -1,11 +1,12 @@
+import numpy as np
 # Convert the year column to numeric
-gapminder.year = pd.to_numeric(gapminder.year)
+gapminder_melt.year = pd.to_numeric(gapminder_melt.year)
 
 # Test if country is of type object
-assert gapminder.country.dtypes == np.object
+assert gapminder_melt.country.dtypes == np.object
 
 # Test if year is of type int64
-assert gapminder.year.dtypes == np.int64
+assert gapminder_melt.year.dtypes == np.int64
 
 # Test if life_expectancy is of type float64
-assert gapminder.life_expectancy.dtypes == np.float64
+assert gapminder_melt.life_expectancy.dtypes == np.float64
