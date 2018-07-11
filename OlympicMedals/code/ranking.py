4fde01b6f13539175579865910fbce081ca0bb54
Noah Lawrence <noahwlawrence@gmail.com>
2018-07-11 10:09:21 -0700

    Create ranking.py
    Creates a simple sorted ranking of countries by medal count and another that includes the medal types as well as the totals

---
+++ OlympicMedals/code/ranking.py
@@ -0,0 +1,18 @@
+import pandas as pd
+medals = pd.DataFrame(pd.read_csv('/Users/noah/Desktop/DFManipulation/Data/summer_1896_2008.csv'))
+
+# To make a new Series for the rankings, the 'NOC' or Name of Country column is selected
+country_names = medals['NOC']
+
+medal_counts = country_names.value_counts()
+
+# This returns the Series of country names with their medal wins counted
+print(medal_counts.head(15))
+
+counted = medals.pivot_table(index='NOC', values='Athlete', columns='Medal', aggfunc='count')
+
+counted['totals'] = counted.sum(axis='columns')
+
+counted = counted.sort_values(by='totals', ascending=False)
+
+print(counted.head(15))
